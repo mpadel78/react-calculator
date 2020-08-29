c0c8bd0368e8a3a90d7daaf050db0aeab5cf57f3
mpadel78 <mpaudel78@gmail.com>
2020-08-29 18:27:27 -0500

    Updates

---
+++ precache-manifest.e92caefbdfd15d003f890d35f7733f3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6d934bb47441ece50c25a59c4f28c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a3249a2610bc4ef0b9a",
+    "url": "/static/css/main.809081e9.chunk.css"
+  },
+  {
+    "revision": "08bfd45894814542bf75",
+    "url": "/static/js/2.f042b241.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f042b241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a3249a2610bc4ef0b9a",
+    "url": "/static/js/main.96eccbba.chunk.js"
+  },
+  {
+    "revision": "ad1dbbc2b8c0432a6a6f",
+    "url": "/static/js/runtime-main.daccfd51.js"
+  }
+]);
