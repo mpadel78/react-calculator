9912ff0fdf0d4d57a3b4b8bc0d82f3fb3ae3322b
Manish Paudel <mpaudel78@gmail.com>
2022-07-27 10:27:04 -0500

    Grommed around

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,70 +1,73 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-import './styles.css'
+import "./styles.css";
 
 function Calculator() {
+  const [data, setData] = useState("");
+  const calcBtns = [];
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach((item) => {
+    calcBtns.push(
+      <button
+        onClick={(e) => {
+          setData(data + e.target.value);
+        }}
+        value={item}
+        key={item}
+      >
+        {item}
+      </button>
+    );
+  });
 
-    const [data, setData]  = useState("");
-    const calcBtns = [];
-    [9,8,7,6,5,4,3,2,1,0, ".", "%"].forEach(item =>{
-        calcBtns.push(
-            <button onClick = {e =>{
-                setData(data+ e.target.value)
-            }}
-            value={item} key ={item}>
-                {item}
-            </button>
-        )
-    })
-
-    return (
-        <div className = "wrapper">
-        <div className = "show-input">{data}</div>
-        <div className= "digits  flex">{calcBtns}</div>
+  return (
+    <>
+      <div style={{ marginTop: "20px" }} className="wrapper">
+        <div className="show-input">{data}</div>
+        <div className="digits  flex">{calcBtns}</div>
         <div className="modifiers subgrid">
-            <button onClick = {()=> setData(data.substr(0, data.length -1))}>
-                Clear
-            </button>
-            <button onClick = {() => setData("")}>
-                AC
-            </button>
+          <button onClick={() => setData(data.substr(0, data.length - 1))}>
+            Clear
+          </button>
+          <button onClick={() => setData("")}>AC</button>
         </div>
 
-        <div className = "operations subgrid">
-            <button onClick  = {e=>setData(data+e.target.value)} value ="+">
-                +
-                </button>
+        <div className="operations subgrid">
+          <button onClick={(e) => setData(data + e.target.value)} value="+">
+            +
+          </button>
 
-                <button onClick  = {e=>setData(data+e.target.value)} value ="-">
-                -
-                </button>
-                <button onClick  = {e=>setData(data+e.target.value)} value ="*">
-                *
-                </button>
-                <button onClick  = {e=>setData(data+e.target.value)} value ="/">
-                /
-                </button>
-<button onClick = {e =>{
-    try{
-        setData(
-            String(eval(data)).length >  3 &&
-            String(eval(data)).includes(".")
-            ? String(eval(data).toFixed(4))
-            :  String(eval(data))
-        )
-    }
-    catch(err){
-        console.log(err)
-    }
-}}
-value = "=">
-    =
-</button>
+          <button onClick={(e) => setData(data + e.target.value)} value="-">
+            -
+          </button>
+          <button onClick={(e) => setData(data + e.target.value)} value="*">
+            *
+          </button>
+          <button onClick={(e) => setData(data + e.target.value)} value="/">
+            /
+          </button>
+          <button
+            onClick={(e) => {
+              try {
+                setData(
+                  String(eval(data)).length > 3 &&
+                    String(eval(data)).includes(".")
+                    ? String(eval(data).toFixed(4))
+                    : String(eval(data))
+                );
+              } catch (err) {
+                console.log(err);
+              }
+            }}
+            value="="
+          >
+            =
+          </button>
         </div>
-        </div>
-    )
+      </div>
 
-
+      <p> Made with ❤️ by Manish </p>
+    </>
+  );
 }
 
-export default Calculator
+export default Calculator;
